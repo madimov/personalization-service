f827ba2b4d5cb61477570cd42225111e0198b537
madimov <mdimov@bu.edu>
2018-02-02 09:58:43 +0100

    add initial tests

---
+++ app/src/test/java/io/interact/personalization/PersonalizationApplicationTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package io.interact.personalization;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mikus
+ *
+ */
+public class PersonalizationApplicationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#getUserActions()}.
+	 */
+	@Test
+	public void testGetUserActions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#setUserActions(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testSetUserActions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#getUserSegments()}.
+	 */
+	@Test
+	public void testGetUserSegments() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#setUserSegments(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testSetUserSegments() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#parseUserActionsData()}.
+	 */
+	@Test
+	public void testParseUserActionsData() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#parseUserSegmentsData()}.
+	 */
+	@Test
+	public void testParseUserSegmentsData() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link io.interact.personalization.PersonalizationApplication#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
